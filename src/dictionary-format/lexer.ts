db5d182a973fd52e739338b2dbbf68979546de87
Robert Massaioli <rmassaioli@atlassian.com>
2023-12-11 16:14:31 +1100

    Getting better at writing the tests and spotting issues in my parsing logic.

--- src/dictionary-format/lexer.ts
+++ src/dictionary-format/lexer.ts
@@ -6,16 +6,19 @@ const ploverEbnfPath = require.resolve('./plover.ebnf');
 
 export function getParser(): Grammars.W3C.Parser {
   const dictionaryOutputBNF = fs.readFileSync(ploverEbnfPath).toString('utf-8');
-  console.log('dictionary content', dictionaryOutputBNF);
   const parser = new Grammars.W3C.Parser(dictionaryOutputBNF);
-  console.log(parser.emitSource());
+  //console.log(parser.emitSource());
   return parser;
 }
 
 export function printAST(ast: IToken): void {
-  printASTHelper(ast, {
+  console.log(toASTString(ast));
+}
+
+export function toASTString(ast: IToken): string {
+  return printASTHelper(ast, {
     depth: 0
-  });
+  }).join('\n');
 }
 
 type PrintASTState = {
@@ -26,8 +29,10 @@ function indent(depth: number): string {
   return '  '.repeat(depth);
 }
 
-function printASTHelper(ast: IToken, state: PrintASTState) {
+function printASTHelper(ast: IToken, state: PrintASTState): Array<string> {
+  const lines = new Array<string>();
   const ind = indent(state.depth);
-  console.log(`${ind}- ${ast.type} (${ast.text})`);
-  ast.children.forEach(child => printASTHelper(child, { ...state, depth: state.depth + 1 }));
+  lines.push(`${ind}- ${ast.type} (${ast.text})`);
+  ast.children.map(child => printASTHelper(child, { ...state, depth: state.depth + 1 })).forEach(result => lines.push(...result));
+  return lines;
 }
