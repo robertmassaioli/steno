8c0280af1bfdd98943da35c40ad041c17878f797
Robert Massaioli <rmassaioli@atlassian.com>
2023-12-12 11:32:28 +1100

    Adding in more and more test cases from plover.
    They all seem to be parsing just fine.

--- src/dictionary-format/lexer.test.ts
+++ src/dictionary-format/lexer.test.ts
@@ -2,7 +2,7 @@ import {describe } from '@jest/globals';
 import { fastDiveSimple, fastDiveText, generateTest } from './lexerTestHelpers';
 
 describe("lexer", () => {
-  describe('simple words', () => {
+  describe('simple text input', () => {
     generateTest('single letter', 'a',
       fastDiveText(['output', 'atom', 'verbatim', 'verbatimSegment', 'verbatimSingle'], 'a')
     );
@@ -13,6 +13,14 @@ describe("lexer", () => {
 
     generateTest('middle spaces', 't e s t',
       fastDiveText(['output', 'atom', 'verbatim'], 't e s t')
+    );
+
+    generateTest('simple numbers', '1-9',
+      fastDiveText(['output', 'atom', 'verbatim'], '1-9')
+    );
+
+    generateTest('simple numbers', '32',
+      fastDiveText(['output', 'atom', 'verbatim'], '32')
     );
   });
 
@@ -58,6 +66,10 @@ describe("lexer", () => {
         fastDiveSimple(['output', 'atom', 'metaCommand', 'metaCommandType', 'attachMetaCommand', 'attachStart'])
       );
 
+      generateTest('simple attach', '{^^}',
+        fastDiveText(['output', 'atom', 'metaCommand', 'metaCommandType', 'attachMetaCommand', 'attachStart'], '^^')
+      );
+
       generateTest('attach symbol correct', '{^}^{^}', {
         type: 'output',
         children: [
@@ -79,6 +91,20 @@ describe("lexer", () => {
       generateTest('attach symbol that starts looking like a stop', '{.^}',
         fastDiveText(['output', 'atom', 'metaCommand', 'metaCommandType', 'attachMetaCommand', 'attachEnd'], '.^')
       );
-    })
+    });
+
+    describe('combinations', () => {
+      generateTest('many different atoms, separated by spaces', '{^} {.} hello {.} {#ALT_L(Grave)}{^ ^}', {
+        type: 'output',
+        children: [
+          fastDiveSimple(['atom', 'metaCommand', 'metaCommandType', 'attachMetaCommand', 'attachStart']),
+          fastDiveSimple(['atom', 'metaCommand', 'metaCommandType', 'stopMetaCommand']),
+          fastDiveText(['atom', 'verbatim'], 'hello'),
+          fastDiveSimple(['atom', 'metaCommand', 'metaCommandType', 'stopMetaCommand']),
+          fastDiveText(['atom', 'metaCommand', 'metaCommandType', 'keyComboMetaCommand', 'singleKeyCombo'], 'ALT_L(Grave)'),
+          fastDiveText(['atom', 'metaCommand', 'metaCommandType', 'attachMetaCommand', 'attachStart'], '^ ^'),
+        ]
+      })
+    });
   });
 });
